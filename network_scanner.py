3e1dd874dfc8d7a8043735cc0397878e843a023c
Evan <erflynn21@gmail.com>
2020-05-07 00:47:47 -0400

    List is parsed and just shows ip and mac addresses from responding devices

--- network_scanner.py
+++ network_scanner.py
@@ -8,7 +8,11 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
-    print(answered_list.summary())
+
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("----------------------------------------------------")
 
 
 scan("10.0.2.1/24")
