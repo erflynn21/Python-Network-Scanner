d0243711b25c8639545c192e27172cf1322b3579
Evan <erflynn21@gmail.com>
2020-05-07 00:16:49 -0400

    Got arguments for request and broadcast into function

--- network_scanner.py
+++ network_scanner.py
@@ -5,7 +5,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 
 scan("10.0.2.1/24")
