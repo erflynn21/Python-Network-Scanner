9727cd1dded1de85ddadd618fcdccd0d81e9f58d
Evan <erflynn21@gmail.com>
2020-05-07 00:38:40 -0400

    List of devices is now printing but not parsed in the terminal

--- network_scanner.py
+++ network_scanner.py
@@ -5,12 +5,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    print(answered_list.summary())
 
 
 scan("10.0.2.1/24")
